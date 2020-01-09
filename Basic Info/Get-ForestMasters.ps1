2dba9ccab1e85126a422aa6955e82063f4947030
LuisRoux3 <33839672+LuisRoux3@users.noreply.github.com>
2020-01-09 12:25:10 +0100

    Add UsrLogins server info and ref in forestmaster

--- Basic Info/Get-ForestMasters.ps1
+++ Basic Info/Get-ForestMasters.ps1
@@ -9,3 +9,24 @@ foreach ($Domain in ((Get-ADForest).domains)) {
 Write-Output ((Get-Date -Format "yyyy/MM/dd HH:mm K") + "`t Forest")
 $MyResult += Get-ADForest | Select-Object Name,SchemaMaster, DomainNamingMaster,InfrastructureMaster, PDCEmulator, RIDMasterall
 $MyResult | Sort-Object DistinguishedName | Out-GridView
+
+<#
+I found later that Patrick Gruenauer did a better job
+https://sid-500.com/2017/06/26/powershell-my-top-10-commands-for-active-directory-documentation-and-monitoring/
+
+#List all Domain-Controllers
+Get-ADDomainController -Filter * | Format-List Name,Ipv4Address,IPv6Address,OperatingSystem
+
+#List all Global Catalog Servers
+Get-ADDomainController -Discover -Service "GlobalCatalog"
+
+#Forest-wide Roles
+Get-ADForest | Format-Table SchemaMaster,DomainNamingmaster
+
+#Domain-wide Roles
+Get-ADDomain | Format-List pdc*,infra*,rid*
+
+netdom query fsmo
+
+Get-EventLog -LogName Security -InstanceId 4624 | Where-Object Message -match "petra" | Format-Table TimeGenerated,Message -AutoSize -Wrap
+#>
--- Basic Info/QuickServerInfo.ps1
+++ Basic Info/QuickServerInfo.ps1
@@ -6,57 +6,62 @@ function Get-BasicInfo {
     $HTMLContent += "<h1>Overview: $Now</h1>"
     
     #Hostname
-    $HTMLContent += "<h2>Hostname</h2>"
+    $HTMLContent += "<h2>Hostname.</h2>"
     $ComputerName = $env:COMPUTERNAME
     $HTMLContent += "<h3>$ComputerName</h3>"
     
     #Hostname
-    $HTMLContent += "<h2>OS Info</h2>"
+    $HTMLContent += "<h2>OS Info.</h2>"
     $HTMLContent += "<h3>OS Name: " + (Get-CimInstance Win32_OperatingSystem).Caption + "</br>"
     $HTMLContent += "OS Version: " + (Get-CimInstance Win32_OperatingSystem).Version + "</br>"
 
     #When Rebooted
-    $HTMLContent += "<h2>Last Boot Uptime (UTC)</h2>"
+    $HTMLContent += "<h2>Last Boot Uptime (UTC).</h2>"
     $LastBootUptime = '{0:u}' -f ((Get-CimInstance Win32_OperatingSystem).LastBootUpTime)
     $HTMLContent += "<h3>$LastBootUptime</h3>"
 
     #Hours UP
-    $HTMLContent += "<h2>Hours since last Boot</h2>"
+    $HTMLContent += "<h2>Hours since last Boot.</h2>"
     $HoursUP = '{0:N2}h' -f((New-TimeSpan -Start ((Get-CimInstance Win32_OperatingSystem).LastBootUpTime) -end (get-date)).TotalHours)
     $HTMLContent += "<h3>$HoursUP</h3>"
     
     #CPU
-    $HTMLContent += "<h2>CPU % used</h2>"
+    $HTMLContent += "<h2>CPU % used.</h2>"
     $CPUUse = "{0:P}" -f ((Get-Counter '\Processor(_Total)\% Processor Time' | Select-Object -ExpandProperty countersamples | Select-Object -Property CookedValue).CookedValue /100)
     $HTMLContent += "<h3>$CPUUse</h3>"
     
     #RAM
-    $HTMLContent += "<h2>RAM % used</h2>"
+    $HTMLContent += "<h2>RAM % used.</h2>"
     $UsedRAM = (Get-Counter '\memory\committed bytes' | Select-Object -ExpandProperty countersamples | Select-Object -Property CookedValue).CookedValue
     $TotalRAM = (Get-Counter '\memory\commit limit' | Select-Object -ExpandProperty countersamples | Select-Object -Property CookedValue).CookedValue
     $RAMUse = "{0:P}" -f ($UsedRAM / $TotalRAM)
     $HTMLContent += "<h3>$RAMUse</h3>"
     
     #Disk space
-    $HTMLContent += "<h2>Disk space</h2>"
+    $HTMLContent += "<h2>Disk space.</h2>"
     $HTMLContent += get-counter '\LogicalDisk(*)\% Free Space' | Select-Object -ExpandProperty countersamples | Where-Object {$_.instancename -like "?:"} | Select-Object -Property instancename,@{L='%Free';E={"{0:P}" -f ($_.CookedValue /100)}} | ConvertTo-HTML -Fragment
     
     #Network
-    $HTMLContent += "<h2>Network IPs</h2>"
+    $HTMLContent += "<h2>Network IPs.</h2>"
     $HTMLContent += Get-NetIPAddress -AddressFamily IPv4 | Sort-Object IPv4Address | Select-Object IPv4Address,InterfaceAlias |  ConvertTo-HTML -Fragment
     
     #Stopped Services
-    $HTMLContent += "<h2>Automatic services stopped</h2>"
+    $HTMLContent += "<h2>Automatic services stopped.</h2>"
     $HTMLContent += Get-Ciminstance win32_service -filter "startmode='auto'" | Where-Object {$_.state -ne 'Running'} | ConvertTo-HTML -Fragment -Property Name,DisplayName,state,startmode
     
     #Tail of errors
-    $HTMLContent += "<h2>Up to 10 Error messages since yesterday</h2>"
+    $HTMLContent += "<h2>Up to 10 Error messages since yesterday.</h2>"
     $Yesterday = (Get-Date) - (New-TimeSpan -Day 1)
     $HTMLContent += Get-WinEvent -FilterHashtable @{LogName='system'; Level=1,2; StartTime=$Yesterday } -maxevents 10 -ErrorAction SilentlyContinue | Sort-Object timecreated -Descending | ConvertTo-HTML -Fragment -Property TimeCreated,ID,message
 
     #Updates without reboot
-    $HTMLContent += "<h2>Updates installed after rebooted</h2>"
+    $HTMLContent += "<h2>Updates installed after rebooted.</h2>"
     $HTMLContent += get-ciminstance -class win32_quickfixengineering | Where-Object {$_.InstalledOn -gt ((Get-CimInstance Win32_OperatingSystem).LastBootUpTime)} | Sort-Object installedon -Descending -ErrorAction SilentlyContinue | ConvertTo-HTML -Fragment -Property InstalledOn,HotFixID,Description,name,Caption
+
+    #Date of "users home"
+    $HTMLContent += "<h2>Folder dates of Users profile sort by access time.</h2>"
+    $UsrRoot = (get-item $env:USERPROFILE).Parent.FullName
+    $HTMLContent += get-childitem $UsrRoot -Attributes Directory | Select-Object FullName,LastAccessTime,CreationTime,LastWriteTime | Sort-Object -Descending LastAccessTime | ConvertTo-HTML -Fragment #-Property InstalledOn,HotFixID,Description,name,Caption
 
     Return $HTMLContent
 }#End function Get-BasicInfo
@@ -94,12 +99,12 @@ function Get-DetailedInfo {
     $HTMLContent += Get-Ciminstance win32_service  | ConvertTo-HTML -Fragment -Property Name,DisplayName,state,startmode
     
     #Last weeks errors
-    $HTMLContent += "<h1>Error messages in last 7 days, System, Application, and setup (max 3k)</h1>"
+    $HTMLContent += "<h1>Error messages in last 7 days, System, Application, and setup (max 300)</h1>"
     $LastWeek = (Get-Date) - (New-TimeSpan -Day 7)
-    $KWinevents = Get-WinEvent -FilterHashtable @{LogName='System'; Level=1,2; StartTime=$LastWeek } -maxevents 1000 -ErrorAction SilentlyContinue
-    $KWinevents += Get-WinEvent -FilterHashtable @{LogName='Application'; Level=1,2; StartTime=$LastWeek } -maxevents 1000 -ErrorAction SilentlyContinue
-    $KWinevents += Get-WinEvent -FilterHashtable @{LogName='Setup'; Level=1,2; StartTime=$LastWeek } -maxevents 1000 -ErrorAction SilentlyContinue
-    $HTMLContent += $KWinevents | Sort-Object timecreated -Descending | ConvertTo-HTML -Fragment -Property TimeCreated,ProviderName,Id,LevelDisplayName,Message
+    $MyWinevents = Get-WinEvent -FilterHashtable @{LogName='System'; Level=1,2; StartTime=$LastWeek } -maxevents 100 -ErrorAction SilentlyContinue
+    $MyWinevents += Get-WinEvent -FilterHashtable @{LogName='Application'; Level=1,2; StartTime=$LastWeek } -maxevents 100 -ErrorAction SilentlyContinue
+    $MyWinevents += Get-WinEvent -FilterHashtable @{LogName='Setup'; Level=1,2; StartTime=$LastWeek } -maxevents 100 -ErrorAction SilentlyContinue
+    $HTMLContent += $MyWinevents | Sort-Object timecreated -Descending | ConvertTo-HTML -Fragment -Property TimeCreated,ProviderName,Id,LevelDisplayName,Message
     
     #Disk
     $HTMLContent += "<h1>Disk detail</h1>"
@@ -141,6 +146,45 @@ function Get-DetailedInfo {
     $HTMLContent += "<h2>Connected ports v2</h2>"
     $HTMLContent += Get-NetTCPConnection | Sort-Object State | ConvertTo-HTML -Fragment
     
+    #Logins
+    $HTMLContent += "<h1>User Logins</h1>"
+    
+    #Local user lastlogon
+    $HTMLContent += "<h2>Local user logins sort by Last Logon</h2>"
+    $HTMLContent += Get-LocalUser | Select-Object name,LastLogon,enabled,AccountExpires,PasswordLastSet,sid,Description | Sort-Object -Descending LastLogon | ConvertTo-HTML -Fragment
+
+    #User's Last logon
+    $HTMLContent += "<h2>All users logins</h2>"
+    $HTMLContent += '<p>Ref: <a href="https://docs.microsoft.com/en-us/windows/security/threat-protection/auditing/audit-logon">Audit logon</a></p>'
+    $HTMLContent += '<p>Ref: <a href="https://docs.microsoft.com/en-us/previous-versions/windows/it-pro/windows-server-2003/cc787567(v=ws.10)?redirectedfrom=MSDN">Audit logon events</a></p>'
+    $HTMLContent += "<table>"
+    $HTMLContent += "<tr><th>TimeCreated</th><th>Event</th><th>Account Name</th><th>Account Domain</th><th>Logon Type</th><th>Elevated Token</th>"
+    
+    $SrcWinevents = Get-WinEvent -FilterHashtable @{LogName='Security'; ID=4624,528; StartTime=$LastWeek } -maxevents 10 | Sort-Object -Descending TimeCreated | Select-Object TimeCreated,Id,message
+    foreach ($OneEvent in $SrcWinevents) {
+        $OneModifMSG = $OneEvent.Message -split "\n\t" #Removes new lines and tab separator
+        $One3AccountNames = ($OneModifMSG | Select-String "Account Name:") 
+        $One3AccountDomains = ($OneModifMSG | Select-String "Account Domain:") 
+        $OneLogonType = ($OneModifMSG | Select-String "Logon Type:") -Split "\t" | Select-Object -Last 1
+        $OneElevationToken = ($OneModifMSG | Select-String "Elevated Token:") -Split "\t" #3 lines
+        $OneElevationToken = ($OneElevationToken[2] -split "\n")[0] #Yes/no"
+        
+        
+        # Time Created and Evemt
+        $OneHTMLContent = "<tr><td>" + ([string]$OneEvent.TimeCreated) + "</td><td>" + ([string]$OneEvent.Id) + "</td><td>"
+        # 3 Account names
+        $OneHTMLContent += [string]$One3AccountNames[0] + "</p>" + [string]$One3AccountNames[1] + "</p>" + [string]$One3AccountNames[2] + "</td><td>"
+        # 3 Account domains
+        $OneHTMLContent += [string]$One3AccountDomains[0] + "</p>" + [string]$One3AccountDomains[1] + "</p>" + [string]$One3AccountDomains[2] + "</td><td>"
+        #Logon type and Elevation token
+        $OneHTMLContent +=  [string]$OneLogonType + "</td><td>" + [string]$OneElevationToken + "</td>"
+
+        $HTMLContent += $OneHTMLContent
+    }
+    
+    $HTMLContent += "</table>"
+
+
     Return $HTMLContent  
 }#End function Get-DetailedInfo
 
